8b4b729b6326f41b8206ec72c85551f23be3f857
Austyn Whaley <austyn_whaley@yahoo.com>
2021-01-16 18:40:29 -0500

    seeds added and functions on server mapped out

--- schema.sql
+++ schema.sql
@@ -10,9 +10,11 @@ CREATE TABLE deparment (
 );
 
 CREATE TABLE role (
-    title VARCHAR(50) NOT NULL,
-    salary DECIMAL(6,2)
+    id INTEGER AUTO_INCREMENT, NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,0)
     department_id INTEGER AUTO_INCREMENT, NOT NULL
+    PRIMARY KEY (id)
 );
 
 
@@ -20,7 +22,74 @@ CREATE TABLE employee (
 id INTEGER AUTO_INCREMENT, NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id
-manager_id
+role_id INTEGER
+manager_id INTEGER
 PRIMARY KEY (id)
 )
+
+
+INSERT INTO department (name) --1
+VALUE ("Offense");
+INSERT INTO department (name) --2
+VALUE ("Defence");
+INSERT INTO department (name) --3
+VALUE ("Special Teams");
+INSERT INTO department (name) --4
+VALUE ("Coaching");
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Quarterback", 39000000, 1);
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Running back", 25000000, 1);
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Long Snapper", 4000000, 3);
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Head Coach", 9000000, 4);
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Assistant Coach", 400000, 4);
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Safety", 5000000, 2);
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Lineman", 2500000, 2);
+
+
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Kicker", 1000000, 3);
+
+
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Tymon", "Wilks", 3, 1);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Taya", "Gordon", null, 2);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Izzy", "Bass", 1, 3);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Blaine", "Allan", 4, 4);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Lester", "Friedman", 4, 5);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("John", "Noble", 2, 6);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jett", "Hulme", null, 7);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Clyde", "May", 1, 8);
+
+--SELECT * FROM department
+
+--SELECT * FROM employee
+
+--SELECT * FROM role
--- server.js
+++ server.js
@@ -2,3 +2,129 @@
 var express = require("express");
 var mysql = require("mysql");
 const cTable = require('console.table');
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "12Aw3309!",
+
+    database: "employee_tracker_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    startingPrompt();
+  });
+  
+
+function startingPrompt() {
+    inquirer.prompt({
+        name: "startMenu",
+        type: 'list',
+        message: "What would you like to do?",
+        choices: [
+
+        "View All Employees?", 
+        "View All Employee's By Roles?",
+        "View all Emplyees By Deparments", 
+        "Update Employee",
+        "Add Employee?",
+        "Add Role?",
+        "Add Department?"]
+    })
+    .then((answer) => {
+        switch(answer.action) {
+            case "View all employees?":
+                viewAllEmployees()
+            break;
+
+            case "View all employees by role?":
+                viewAllRoles() 
+            break;
+
+            case "View all employees by department?":
+                viewAllDepartments() 
+            break;
+
+            case "Update employee":
+                updateEmployee();
+            break;
+
+            case "Add employee?":
+                addEmployee()
+            break;
+
+            case "Add role?":
+                addRole();
+            break;
+
+            case "Add department?":
+                addDepartment()
+            break;
+
+        };
+    });
+};
+  
+function viewAllEmployees() {
+
+    inquirer.prompt({
+
+
+    });
+    
+};
+
+
+function viewAllRoles() {
+
+};
+
+function viewAllDepartments() {
+
+};
+
+function updateEmployee() {
+
+};
+
+function addEmployee() {
+    inquirer.prompt(
+    {
+        name:"firstName",
+        type:"input",
+        message:"What is the employee's first name?"
+    },
+    {
+        name:"lastName",
+        type:"input",
+        message:"What is the employee's last name?"
+    },
+    {
+        name:"role",
+        type:"list",
+        message:"What is the employee's role?",
+        choices:["Offense", "Defence","Special Teams","Coaching"]
+    },
+    {
+        name:"manager",
+        type:"list",
+        message:"Who is the employee's manager?",
+        choices:[]
+    }
+    );
+
+};
+
+function addDepartment() {
+
+};
+
+function addRole() {
+
+};
